8a3b7433934de2b952c620be5f06f6a8809fd95d
Benjie Jiao <hi@benjie.me>
2017-12-09 02:58:02 +0800

    Added a menu page

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Earthquake Cluster Map' });
+  res.render('index', { title: 'Historical Earthquakes - Main Menu' });
+});
+
+/* GET home page. */
+router.get('/clusters', function(req, res, next) {
+  res.render('clusters', { title: 'Historical Earthquakes - Cluster Map' });
 });
 
 router.get('/heatmap', function(req, res, next) {
-  res.render('heatmap', { title: 'Earthquake Heatmap' });
+  res.render('heatmap', { title: 'Historical Earthquakes - Heatmap' });
 });
 
 router.get('/timelapse', function(req, res, next) {
-  res.render('timelapse-leaflet', { title: 'Earthquake Timelapse' });
+  res.render('timelapse-leaflet', { title: 'Historical Earthquakes - Timelapse' });
 });
 
 module.exports = router;
